6e0cf50ee542f4962761d8e06ae936b5b80ed3b9
CutieCat2804 <cutiecattv@gmail.com>
2023-09-21 21:10:53 +0200

    added table loottables / recipes

--- src/main/java/github/cutiecat2804/farmhousefurniture/datagen/ModBlockTagProvider.java
+++ src/main/java/github/cutiecat2804/farmhousefurniture/datagen/ModBlockTagProvider.java
@@ -23,6 +23,11 @@ public class ModBlockTagProvider extends BlockTagsProvider {
         this.tag(BlockTags.MINEABLE_WITH_PICKAXE).add(BlockInit.EXAMPLE_BLOCK.get());
         this.tag(BlockTags.MINEABLE_WITH_AXE).add(BlockInit.NEWSPAPER_STAND.get());
 
+        this.tag(BlockTags.MINEABLE_WITH_AXE).add(BlockInit.BLUE_WOOD_TABLE.get());
+        this.tag(BlockTags.MINEABLE_WITH_AXE).add(BlockInit.OAK_WOOD_TABLE.get());
+        this.tag(BlockTags.MINEABLE_WITH_AXE).add(BlockInit.DARK_WOOD_TABLE.get());
+        this.tag(BlockTags.MINEABLE_WITH_AXE).add(BlockInit.GREY_WOOD_TABLE.get());
+
         this.tag(BlockTags.NEEDS_IRON_TOOL).add(BlockInit.EXAMPLE_BLOCK.get());
 
     }
